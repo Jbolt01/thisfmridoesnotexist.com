caaf2143a59c6c0e54c86286b7e87d80e6e24151
notewio <fzheng413@gmail.com>
2022-03-29 11:54:50 -0400

    this is a bad idea

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
-const IMG_SIZE = 200;
+const IMG_SIZE = 150;
+const IMG_HEIGHT = 180;
 const scroll = { x: 0, y: 0 };
 let images = [];
 
@@ -14,11 +15,17 @@ function resize() {
 function draw() {
   for (let x = -IMG_SIZE, i = 0; x < canvas.width; x += IMG_SIZE, i += 1) {
     if (i >= images.length) { images.push([]) }
-    for (let y = -IMG_SIZE, j = 0; y < canvas.height; y += IMG_SIZE, j += 1) {
+    for (let y = -IMG_HEIGHT, j = 0; y < canvas.height; y += IMG_HEIGHT, j += 1) {
       if (j >= images[i].length) { images[i].push(new Image()) }
       // TODO: receive images from API
-      if (images[i][j].src === "") { images[i][j].src = "eepy.png" }
-      ctx.drawImage(images[i][j], x + scroll.x, y + scroll.y, IMG_SIZE, IMG_SIZE);
+      if (images[i][j].src === "") {
+        images[i][j].src = `data/image.${
+          ("0000"+Math.floor(Math.random() * 150 + 1)).slice(-4)
+        }.${
+          ("0000"+Math.floor(Math.random() * 45 + 1)).slice(-4)
+        }.png`;
+      }
+      ctx.drawImage(images[i][j], x + scroll.x, y + scroll.y, IMG_SIZE, IMG_HEIGHT);
     }
   }
   
@@ -29,12 +36,12 @@ function draw() {
     images.splice(0, 0, []);
     scroll.x %= IMG_SIZE;
   }
-  if (scroll.y >= IMG_SIZE) {
+  if (scroll.y >= IMG_HEIGHT) {
     images.forEach(e => {
       e.pop();
       e.splice(0, 0, new Image());
     });
-    scroll.y %= IMG_SIZE;
+    scroll.y %= IMG_HEIGHT;
   }
 }
 
